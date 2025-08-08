6cd415df961f5c49850f53941442e7b245e23057
Kukki777 <priyansh23042004@gmail.com>
2025-08-08 23:45:39 +0530

    userSchema

---
+++ modal/user.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    phoneNumber: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    OTP: {
+        type: String,
+    },
+    verified: {
+        type: Boolean,
+        default: false
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+    },
+    userToken: {
+        type: String,
+        default: null,
+    },
+    age : {
+        type: Number,
+        required: true
+    }
+
+}, {timestamps: true});
+
+const userSchemasave = mongoose.model.userSchemasave || mongoose.model('userrSchemasave', userSchema)
+
+export default userSchemasave;
