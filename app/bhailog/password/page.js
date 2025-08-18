e30a5310f53ab73b780cfa1e94047aa74b19759c
saurabh4323 <saurabhiitr01@gmail.com>
2025-08-19 01:04:11 +0530

    rr

--- app/bhailog/password/page.js
+++ app/bhailog/password/page.js
@@ -1,6 +1,5 @@
 "use client";
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Image from "next/image";
 import { Lock, Eye, EyeOff, Shield, Zap, ArrowRight } from "lucide-react";
